e81eb4b3c092a2773179e5b22b0065f8082fe175
funcccc-gh <milo12@qq.com>
2026-09-19 09:55:14 +0800

    内测3次提交 测试

--- RLan/RLan/build.gradle.kts
+++ RLan/RLan/build.gradle.kts
@@ -41,3 +41,13 @@ java {
 application {
     mainClass = "rlan.Main"
 }
+
+tasks.jar {
+    archiveClassifier.set("all")
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    exclude("module-info.class")
+    manifest {
+        attributes["Main-Class"] = "rlan.Main"
+    }
+}
